22fc5bd775ae2c349bb4899d55891b79fe2d530d
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-08-09 17:16:45 -0300

    pokedex-express-postgres-03

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,9 @@ const routes = require("express").Router();
 const PokemonController = require("../controllers/PokemonController");
 routes.get("/", PokemonController.getAll);
 routes.get("/signup", PokemonController.signup);
+routes.post("/create", PokemonController.create);
+routes.get("/getById/:id/:method", PokemonController.getById);
+routes.post("/update/:id", PokemonController.update);
+routes.get("/remove/:id", PokemonController.remove);
 
 module.exports = routes;
