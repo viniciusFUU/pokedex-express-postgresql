c161a7fa73fc9c574319abd1808652beaf113b19
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-08-11 17:07:32 -0300

    pokedex-express-postgresql-06

--- public/script.js
+++ public/script.js
@@ -11,4 +11,32 @@ const closeAlert = () => {
   }, 5000);
 };
 
+const viewDropdowm = () => {
+  const button = document.querySelectorAll(".dropdown-button");
+
+  button.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const content = event.path[2].children[1];
+
+      content.classList.toggle("active");
+
+      if (content.classList.contains("active")) {
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
+      }
+
+      content.addEventListener("mouseleave", () => {
+        content.classList.remove("active");
+
+        if (!content.classList.contains("active")) {
+          content.style.display = "none";
+        }
+      });
+    });
+  });
+};
+
+viewDropdowm();
+
 closeAlert();
